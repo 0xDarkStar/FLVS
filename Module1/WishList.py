9249a4dc7537db957f30f8a6c08e56f61a1223fe
0xDarkStar <111785550+0xDarkStar@users.noreply.github.com>
2022-09-23 16:54:37 -0400

    Update WishList.py

--- Module1/WishList.py
+++ Module1/WishList.py
@@ -1,1 +1,7 @@
-
+"""
+1. Make a list of items for the user to choose
+2. Inform user that they are to choose 3 or more items from the list
+3. Add up the prices and shipping costs of all the items they chose
+4. Tell them the cost of everything they chose
+all prices and shipping costs are coming from Amazon
+"""
