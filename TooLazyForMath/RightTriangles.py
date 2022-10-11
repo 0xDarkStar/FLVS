2fc65eb9a37ea71cefb8d0188b5f75430bd3a331
0xDarkStar <111785550+0xDarkStar@users.noreply.github.com>
2022-10-11 17:49:25 -0400

    Fixed something and moved the code around.
    The print statement in the code to check if it was a right triangle or not was in the wrong place. I also replaced the code that looked for the hypotenuse with the code that checks if it's a right triangle since they both give the same result (if the hypotenuse given isn't correct)

--- TooLazyForMath/RightTriangles.py
+++ TooLazyForMath/RightTriangles.py
@@ -9,11 +9,11 @@ import math, time
 
 #Variables for the sides. If you don't have the measurment for that side, leave a "?". Also, don't add "inches", "in.", "cm", "centimeters", etc.
 
-print("I will ask you for the sizes of both legs and the hypotenuse, if you don't have it, just put '?'. \nAlso! Don't include stuff like inches or cm, you would break the code.")
+print("I will ask you for the sizes of both legs and the hypotenuse, if you don't have it, just put '?'. \nAlso! Don't include stuff like inches or cm, you would break the code.") 
 time.sleep(2)
-Leg1 = input("What is the size of leg 1? ")
+Leg1 = input("\nWhat is the size of leg 1? ")
 Leg2 = input("What is the size of leg 2? ")
-Hypot = input(" what is the size of the hypotenuse? ")
+Hypot = input("What is the size of the hypotenuse? ")
 
 #Make sure that the program doesn't run with only one measurement since it won't work with only one (duh). It would also work if all three are "?"
 if Leg1 == "?" and Leg2 == "?" or Leg2 == "?" and Hypot == "?" or Leg1 == "?" and Hypot == "?":
@@ -21,39 +21,34 @@ if Leg1 == "?" and Leg2 == "?" or Leg2 == "?" and Hypot == "?" or Leg1 == "?" an
     exit()
 
 #Now that we know the user has 2 or more variables filled in then we can start doing the math
-#Let's start with finding the hypotenuse
-if Leg1 != "?" and Leg2 != "?"  and Hypot == "?":
-    print("\nI am looking for the hypotenuse")
-    print("Leg 1 is '" + str(Leg1) + "' and Leg 2 is '" + str(Leg2) + "'.")
-    Hypot = math.sqrt(float(Leg1) ** 2 + float(Leg2) ** 2)
-    print("\nThe hypotenuse is " + str(Hypot))
-    print("\nI hope this helped!")
+#If the have both legs, it'll tell them the hypotenuse
+if Leg1 != "?" and Leg2 != "?":
+    print("\nI will check if that can be a right triangle. If it isn't, I'll tell you the correct hypotenuse.")
+    print("Leg 1 is '" + str(Leg1) + "' and leg 2 is '" + str(Leg2) + "'.")
+    x = math.sqrt(float(Leg1) ** 2 + float(Leg2) ** 2)
+    y = round(x, 2) #Nobody would want to type an extremely long number as their answer right? You'd want to round to the hundredths place most of the time.
+    if y == Hypot or int(y) == Hypot or x == Hypot:
+        print("\nThat is a right triangle.")
+        print("\nI hope this helped!")
+    elif Hypot == "?" or Hypot == "":
+        print("The hypotenuse would be '" + str(x) + "'.")
+        print("\nI hope this helped!")
+    else:
+        print("\nIt isn't a right triangle.")
+        print("The hypotenuse is '" + str(x) + "'.")
+        print("\nI hope this helped!")
 
-#If they had the hypotenuse then we'll look using that
+#If they have a hypotenuse and one leg we'll look for the missing leg
 elif Hypot != "?" and Leg1 != "?" and Leg2 == "?" or Hypot != "?" and Leg2 != "?" and Leg1 == "?":
-    if Leg1 == "?":
+    if Leg1 == "?": #Leg 1 is missing
         print("\nI will look for Leg 1")
         print("Leg 2 is '" + str(Leg2) + "' and the hypotenuse is '" + str(Hypot) + "'.")
         Leg1 = math.sqrt(float(Hypot) ** 2 - float(Leg2) ** 2)
         print("\nLeg 1 is '" + str(Leg1) + "'.")
         print("\nI hope this helped!")
-    else:
+    else: #Leg 2 is missing
         print("\nI will look for Leg 2")
         print("Leg 1 is '" + str(Leg1) + "' and the hypotenuse is '" + str(Hypot) + "'.")
         Leg2 = math.sqrt(float(Hypot) ** 2 - float(Leg1) ** 2)
         print("\nLeg 2 is '" + str(Leg2) + "'.")
-        print("\nI hope this helped!")
-
-#If they have all the measurements then we can check if they are correct or not
-elif Leg1 != "?" and Leg2 != "?" and Hypot != "?":
-    print("\nI will check if that can be a right triangle")
-    print("Leg 1 is '" + str(Leg1) + "', leg 2 is '" + str(Leg2) + "', and the hypotenuse is '" + str(Hypot) + "'.")
-    x = math.sqrt(float(Leg1) ** 2 + float(Leg2) ** 2)
-    y = round(x, 2) #Nobody would want to type an extremely long number as their answer right? You'd want to round to the hundredths place most of the time.
-    if y == Hypot or int(y) == Hypot or x == Hypot:
-        print("\nThat is a right triangle.")
-        print("The hypotenuse would be '" + str(x) + "'.")
-        print("\nI hope this helped!")
-    else:
-        print("\nIt isn't a right triangle.")
         print("\nI hope this helped!")
